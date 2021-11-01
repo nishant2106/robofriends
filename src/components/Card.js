ea6094e5f0b790e41dd0c483b0d45c2c3afb825a
Nishant Pandey <nishantpandey2106@gmail.com>
2021-11-01 09:33:14 +0530

    final commit

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = ({name,email,id}) => {
+    return (
+        <div className = "tc bg-light-green dib br3 pa3 ma2 grow bw-2 shadow-5">
+            <img alt = "robots" src = {`https://robohash.org/${id}?200x200`}></img>
+            <div>
+                <h2>{name}</h2>
+                <h3>{email}</h3>
+            </div>
+        </div>
+    )
+}
+export default Card;
